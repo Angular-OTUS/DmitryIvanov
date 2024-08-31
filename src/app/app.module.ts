ad3b8282e23df1b2b4afa2a09fbfda4a78543642
Dmitry Ivanov <dmitry.ivanov@kub3.ru>
2024-08-31 16:34:17 +0200

    ДЗ2 Взаимодействие между компонентами

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { FormsModule } from '@angular/forms';
+import { ToDoListItemComponent } from './components/to-do-list-item/to-do-list-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToDoListComponent
-  ],
+  declarations: [AppComponent, ToDoListComponent, ToDoListItemComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
